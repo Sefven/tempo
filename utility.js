43c716ff214c41a9f60cf51a2de3d0eacb73bbae
youssef <sefennec@gmail.com>
2017-03-06 14:09:50 +0100

    edit index.html

--- utility.js
+++ utility.js
@@ -1,3 +1,17 @@
 var log, byId, select, selectAll;
 
-log = function byId(log)
+log = function byId(log); {
+    "use strict";
+    return
+};
+
+
+byId = function byId(id) {
+    "use strict";
+    return document.getElementById(id);
+};
+
+select = function select(sCSS) {
+    "use strict";
+    return document.querySelectorAll(sCSS);
+}
